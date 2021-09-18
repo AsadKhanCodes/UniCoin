3e73b46d2731134bddfb352096e4913cdc12fc2d
Asad Khan <asad.khan@tutanota.com>
2021-09-18 13:44:31 -0400

    Reconfigure Truffle Directories

--- src/crypto/truffle-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  networks: {
-      development: {
-          host: "127.0.01", // Configs truffle to run on local host
-          port: "7545",
-          network_id: "*"
-      }
-  }
-}
