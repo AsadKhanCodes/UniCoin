a1c9b44f4743f11c735f4fdc45cbe688a4f5a67a
Asad Khan <asad.khan@tutanota.com>
2021-09-18 13:02:49 -0400

    Create Truffle Config

---
+++ src/crypto/truffle.js
@@ -0,0 +1,9 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.01", // Configs truffle to run on local host
+            port: "7545",
+            network_id: "*"
+        }
+    }
+}
