135a2846ed257e05006d8d0aceb3ea9e710e5335
Asad Khan <asad.khan@tutanota.com>
2021-09-18 17:14:25 -0400

    Transfer Coin Solution from Remix

---
+++ src/crypto/Coin.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.7;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract UniCoin is ERC20 {
+    
+    address public admin;
+    
+    constructor() ERC20('UniCoin', 'UNI') {
+        _mint(msg.sender, 1000000000000 * 10 ** 18);
+        admin = msg.sender;
+    }
+    
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount);
+    }
+    
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
